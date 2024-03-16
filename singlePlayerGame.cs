9651645b73284ff2afbe48bf05bc1441a33b7319
saikatCV <saikat030703@gmail.com>
2024-03-16 11:30:42 +0530

    uc1 : added the class for the single player game

---
+++ singlePlayerGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    public class singlePlayerGame
+    {
+        public string name;
+        public singlePlayerGame()
+        {
+            Console.WriteLine("Enter the name\n ");
+            string name = Console.ReadLine();
+            this.name = name;
+
+
+
+        }
+
+       
+    }
+}
