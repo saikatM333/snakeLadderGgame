978ae0bf3c2d45be24e2b5b9cf86c225124df558
saikatCV <saikat030703@gmail.com>
2024-03-16 11:40:15 +0530

    Uc3 : added the functionality in which it will dicide the noplay ladder or snake based upon which it will change the position

--- singlePlayerGame.cs
+++ singlePlayerGame.cs
@@ -33,8 +33,74 @@ namespace SnakeAndLadderGame
             {
                 int roll = r.Next(1, 7);
 
+                position = position + roll;
+                // it will call the function to decide noplay, snale , ladder 
+                if (!(position >= 94))
+                {
+                    position = decidingtheStatus(position);
+
+                }
+// this will ensure that position will not go above 100
+                if (position > 100)
+                {
+                    position = position - roll;
+                }
+                // this will count the number of times dice is rolled 
+                numberOfTimesDieRolled++;
+                Console.WriteLine($"the position of {this.name} is {position}");
+            }
+            
+        }
+        public int decidingtheStatus(int position)
+        {
+
+            Random r = new Random();
+            int status = r.Next(0, 3);
+            int change = 0;
+            if (status == 0)
+            {
+                return position - change;
+            }
+            else if (status == 1)
+            {
+                if (position <= 95)
+                {
+                    return position - snakeLength();
+                }
+
+                else
+                {
+                    return position;
+                }
+            }
+            else
+            {
+                if (position <= 64)
+                {
+
+                    return position + ladderLength();
+
+
+                }
+                else return position;
+
 
             }
         }
+
+        public int snakeLength()
+        {
+            Random r = new Random();
+            return r.Next(0, 12);
+
+        }
+
+        public int ladderLength()
+        {
+            Random r = new Random();
+            return r.Next(0, 13);
+        }
+
     }
+
 }
