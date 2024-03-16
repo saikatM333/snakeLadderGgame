cb2f35b279e8bbb8be2b8bc90eb004998dd48511
saikatCV <saikat030703@gmail.com>
2024-03-16 11:26:16 +0530

    added the main function from where the different class is called

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SnakeAndLadderGame;
+using SnakeLadderGame;
+
+Console.WriteLine("Snake and ladder game ");
+int select = Convert.ToInt32(Console.ReadLine());
+switch (select) {
+    case 0:
+        singlePlayerGame s = new singlePlayerGame();
+s.playingTheGame();
+        break;
+        case 1:
+        twoplayergame game = new twoplayergame();
+        game.playingTheGame();
+        break;
+    default: Console.WriteLine("pls select between 0 and 1 ");
+        break;
+}
