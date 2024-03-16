33cab8532dab347db7c9a5abca3bbb205bb9e057
saikatCV <saikat030703@gmail.com>
2024-03-16 11:41:50 +0530

    uc4 : added the functionality which will check the coindition whether the position of player should not go above 100

--- singlePlayerGame.cs
+++ singlePlayerGame.cs
@@ -40,7 +40,15 @@ namespace SnakeAndLadderGame
                     position = decidingtheStatus(position);
 
                 }
-// this will ensure that position will not go above 100
+                //it will ensure that player win only when position is 100
+                if (position == 100)
+                {
+                    Console.WriteLine($"{this.name}  win the game ");
+                    gameStatus = false;
+                    break;
+                }
+                
+                // this will ensure that position will not go above 100
                 if (position > 100)
                 {
                     position = position - roll;
